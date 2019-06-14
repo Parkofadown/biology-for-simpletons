2e86e8e98f9e731832d1a6e676b5d1ffff33581f
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-06-14 16:28:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/')  
+
 @app.route('/index') # 127.0.0.1:5000
 def index():
     return render_template('index.html')
